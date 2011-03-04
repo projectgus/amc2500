6fef60fc13452deec04cd4f00f1403cde4b33153
Angus Gratton <angus@projectgus.com>
2011-03-04 18:59:24 +1100

    Rewrite to support dispatching on class methods

--- visitor.py
+++ visitor.py
@@ -12,38 +12,41 @@ class ClassB(object):
     def __init__(self):
         pass
 
-registry = {}
+methods = {}
 
-class Visitor(object):
-    def __init__(self, fun_name):
-        self.fun_name = fun_name
-        self.typemap = {}
+class when(object):
+    def __init__(self, argtype):
+        self.argtype = argtype
+    def __call__(self, func):
+        print "assigning %s to func %s" % (self.argtype, func)
+        self.func_name =func.__name__
+        if not self.func_name in methods:
+            methods[self.func_name] = method_overload(self.func_name)        
+        methods[self.func_name].register(self.argtype, func)
+        return methods[self.func_name]
+
+# this is the actual overload of a specific method name
+class method_overload(object):
+    def __init__(self, func_name):
+        self.func_name = func_name
+        self.registry = {}
+
+    def register(self, argtype, func):
+        self.registry[argtype] = func
+
+    def __get__(self, obj, type=None):
+        print "method_overload.__get__ %s %s" % (obj, type)
+        self.obj = obj
+        self.type = type
+        return self
+
     def __call__(self, *args, **kw):
-        function = self.typemap.get(type(args[1]))
-        if function is None:
-            raise TypeError("no match")
-        return function(*args, **kw)
-    def register(self, argtype, function):
-        if argtype in self.typemap:
-            raise TypeError("duplicate registration")
-        self.typemap[argtype] = function
-
-
-def when(argtype):
-    def register(function):   
-        print function.__class__
-        print dir(function)
-        #print function.im_self
-        print dir(function.func_code)
-        print inspect.getargspec(function)
-        name = function.__name__
-        if not name in registry:
-            mm = registry[name] = Visitor(name)        
-        mm = registry.get(name)
-        #inspect.getargspec(function)[0]
-        mm.register(argtype, function)
-        return mm
-    return register
+        argtype = type(args[0])
+        if not argtype in self.registry:
+            raise TypeError("Function %s has no overload registered for type %s" % 
+                            (self.func_name, argtype))
+        func = self.registry[argtype]
+        func.__get__(self.obj, self.type)(*args, **kw)
 
 
 class Sample:
